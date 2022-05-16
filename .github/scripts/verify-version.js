2c4b05ab5d1c3728f5cdfd9c6f81d09076696371
Devecor <devecor@outlook.com>
2022-05-16 19:40:31 +0800

    add verify version workflow

---
+++ .github/scripts/verify-version.js
@@ -0,0 +1,49 @@
+const {readFileSync} = require("fs")
+const {execSync} = require("child_process")
+
+const argv = require('minimist')(process.argv.slice(2));
+
+console.log(argv)
+
+const ver = read_version()
+const dst = get_target_version()
+verify(ver, dst)
+
+
+function read_version_from_pure_text(path) {
+    return readFileSync(path, "utf8")
+}
+
+function read_version_from_package_json(path) {
+    return JSON.parse(readFileSync(path, "utf8")).version
+}
+
+function read_version() {
+    if (argv?.platform === "pure-txt") {
+        return read_version_from_pure_text(argv?.path)
+    } else if (argv?.platform === "nodejs") {
+        return read_version_from_package_json(argv?.path)
+    } else {
+        throw Error(`platform: ${argv?.platform} is unknown`)
+    }
+}
+
+function get_target_version() {
+
+    if (argv?.destination === "git-tag") {
+        return String.fromCharCode(...execSync("git tag -l")).trim().split("\n").at(-1)
+    } else {
+        throw Error(`destination: ${argv?.destination} is unknown`)
+    }
+}
+
+function verify(src, dst) {
+    if (src.length !== dst.length) {
+        throw Error(`we can't compare ${src} with ${dst}`)
+    }
+    if (src <= dst) {
+        throw Error("please don't forget to bump version.")
+    } else {
+        console.log(src)
+    }
+}
